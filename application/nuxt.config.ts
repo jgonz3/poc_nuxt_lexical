105e213015ec02d2d3bdc43a8def9dc6d78e3a20
Gyen Abubakar <gyenabubakar@gmail.com>
2024-02-25 11:50:39 +0000

    code cleanup

--- application/nuxt.config.ts
+++ application/nuxt.config.ts
@@ -10,20 +10,6 @@ export default defineNuxtConfig({
   imports: {
     autoImport: false,
   },
-  // css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      'postcss-import': {},
-      'tailwindcss/nesting': {},
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  modules: ['shadcn-nuxt'],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui',
-  },
   alias: {
     'lexical-vue-editor': path.resolve(__dirname, '../lexical-vue-editor/src'),
     'lexical-vue': path.resolve(__dirname, '../lexical-vue/src'),
